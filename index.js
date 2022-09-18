354e6999cbb3541ac6466a1e8be36a97403d909d
Pharma2022 <111680056+Pharma2022@users.noreply.github.com>
2022-09-18 09:46:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+// javascript
+
+
+
+
+const inputEl = document.getElementById("input-el")
+const convertEl=document.getElementById("convert-el")
+const lengthEl=document.getElementById("length-el")
+const volumeEl=document.getElementById("volume-el")
+const massEl=document.getElementById("mass-el")
+
+
+const myArray=[lengthEl,volumeEl,massEl]
+const myConversionFactor=[3.281,0.264,2.204]
+const myMetric=["metres","litres","kilograms"]
+const myImperial=["feet","gallons","pounds"]
+const myUnitString=["Length(Metres/Feet)", "Volume(Litres/Gallons)", "Weight,(Kilograms/Pounds)"]
+
+
+let inputValue = 0
+render(myArray,myConversionFactor,myMetric,myImperial,myUnitString,inputValue)
+
+
+function render(arr,cVf,metric,imperial,unitString,inputValue){
+ inputValue= inputEl.value   
+    for (let i=0; i<arr.length; i++){
+        let multiply= inputValue*cVf[i]
+        let divide= inputValue/cVf[i]
+    
+      arr[i].innerHTML=  `<p>  ${unitString[i]}</p>
+      <p>${inputValue} ${metric[i]} is ${multiply.toFixed(3)} ${imperial[i]} 
+       ${inputValue} ${imperial[i]} is ${divide.toFixed(3)} ${metric[i]}
+       </p>`
+           
+    }
+    
+    
+}
+convertEl.addEventListener("click", function(){
+
+render(myArray,myConversionFactor,myMetric,myImperial,myUnitString,inputValue)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
